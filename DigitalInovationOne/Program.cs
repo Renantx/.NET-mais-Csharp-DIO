328d43592f35bb76803345758c0426b4c5f4b75c
Renantx <66394069+Renantx@users.noreply.github.com>
2021-08-12 13:59:19 -0300

    Add files via upload

---
+++ DigitalInovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 6;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Teste de aplicação .NET {i}");
+            }
+        }
+    }
+}
