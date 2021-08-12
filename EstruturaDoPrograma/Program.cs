424ffe4c59486e619158544047b36c9ba469f90b
Renantx <66394069+Renantx@users.noreply.github.com>
2021-08-12 14:00:03 -0300

    Add files via upload

---
+++ EstruturaDoPrograma/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EstruturaDoPrograma.Exemplos;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(2);
+            s.Empilha(3);
+            s.Empilha(4);
+            s.Empilha(5);
+            s.Empilha(6);
+            s.Empilha(7);
+            s.Empilha(8);
+            s.Empilha(9);
+            s.Empilha(10);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
